379acdeb05905a7d121b117aaf782104d90c9e29
sebas-baltor <sebastian_1321124218@uptecamac.edu.mx>
2023-05-04 20:17:17 -0600

    adding handles to show and hidden the floatin menu

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -4,16 +4,20 @@ import { BsPersonCircle, BsFillPeopleFill } from "react-icons/bs";
 import { BiExit } from "react-icons/bi";
 import Image from "next/image";
 import logo from "@/public/tweeter.svg"
+import { Poppins, Noto_Sans } from "next/font/google"
+import {useRef} from "react";
+const poppins = Poppins({weight:"500",style:"normal",subsets:["latin"]})
+const notoSans = Noto_Sans({weight:"500",style:"normal",subsets:["latin"]})
 export default function Navbar() {
+  const floatingMenu = useRef<HTMLDivElement>(null);
   return (
-    <nav className="w-full fixed z-10 top-0 right-0 flex items-center justify-between px-6 py-4 bg-white shadow-md">
+    <nav className={`${poppins.className} w-full fixed z-10 top-0 right-0 flex items-center justify-between px-6 py-4 bg-white shadow-md`}>
       <div>
         <Image
           src={logo}
           alt="Logo de la empresa"
           width={150}
           height={30}
-          layout="responsive"
           className="h-8"
         />
       </div>
@@ -39,7 +43,9 @@ export default function Navbar() {
         </a>
       </div>
       <div className="relative">
-        <div>
+        <div onClick={()=>{
+          floatingMenu.current?.classList.toggle("hidden");
+        }}>
           <button
             type="button"
             className="flex items-center focus:outline-none gap-3"
@@ -52,7 +58,7 @@ export default function Navbar() {
                 className="h-8 w-8 rounded-lg object-cover"
               />
             </div>
-            <div className="hidden lg:flex items-center">
+            <div className={`${notoSans.className} hidden lg:flex items-center`}>
               <span className="truncate">Nombre de usuario</span>
               <svg className="w-3 h-3 ml-1 fill-current" viewBox="0 0 12 12">
                 <path d="M2.391,4.301c0.357-0.356,0.93-0.356,1.286,0l2.047,2.047l2.047-2.047c0.356-0.356,0.93-0.356,1.286,0 c0.357,0.357,0.357,0.93,0,1.287L7.021,8.635c-0.178,0.178-0.411,0.268-0.643,0.268c-0.232,0-0.465-0.089-0.643-0.268 L2.391,5.588C2.034,5.231,2.034,4.658,2.391,4.301z" />
@@ -60,7 +66,7 @@ export default function Navbar() {
             </div>
           </button>
         </div>
-        <div className="absolute right-0 top-[200%] w-40 py-2 p-3 bg-white rounded-md shadow-md z-10 flex flex-col gap-1">
+        <div ref={floatingMenu} className={`${notoSans.className} absolute right-0 top-[200%] w-40 py-2 p-3 bg-white rounded-md shadow-md z-10 flex flex-col gap-1`}>
           <a
             href="/profile/1"
             className="flex gap-4 flex-nowrap items-center px-4 py-2 text-sm text-gray-600 hover:bg-gray-100 hover:text-blue-600 rounded-md"
