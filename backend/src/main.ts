01f0e153bae0614517678bda71543272cc595e95
sebas-baltor <sebastian_1321124218@uptecamac.edu.mx>
2023-06-06 18:20:14 -0600

    swagger implemented

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+  .setTitle("Twitter clone api")
+  .setDescription("Based on devchallenge's challenge we create and api to comunicate and store data like Twitter does")
+  .setVersion("1.0")
+  .addTag("tweeter").addBearerAuth()
+  .build();
   app.useGlobalPipes(new ValidationPipe())
+  const document = SwaggerModule.createDocument(app,config);
+  SwaggerModule.setup("api-documentation",app,document);
   await app.listen(3001);
 }
 bootstrap();
