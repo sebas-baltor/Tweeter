49ea330a9fa7296780165b6ca3afe57fa5fd19ed
sebas-baltor <sebastian_1321124218@uptecamac.edu.mx>
2023-06-07 16:31:14 -0600

    remove the inithial path hellow

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -2,12 +2,4 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    console.log(process.env.JWT_SECRET_KEY)
-    return this.appService.getHello();
-  }
-}
+export class AppController {}
