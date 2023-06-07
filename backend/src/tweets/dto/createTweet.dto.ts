d0b5360abfec807200a92a7f0c37e503647b5c44
sebas-baltor <sebastian_1321124218@uptecamac.edu.mx>
2023-06-07 16:31:48 -0600

    add ApiProperties to swagger

--- backend/src/tweets/dto/createTweet.dto.ts
+++ backend/src/tweets/dto/createTweet.dto.ts
@@ -1,17 +1,23 @@
-import { IsNotEmpty,IsBooleanString,} from "class-validator";
+import { IsNotEmpty,IsBooleanString} from "class-validator";
+import { Tweet } from "../schema/tweet.schema";
+import { ApiProperty } from "@nestjs/swagger";
 export class CreateTweetDto {
+    @ApiProperty({required:true,type:"ObjectId or string",description:"identify the current user"})
     @IsNotEmpty()
     from:string;
-
+    @ApiProperty({required:true,default:"true",type:"boolean string",example:"true",description:"represent the tweet's visivility true=everyone, false=just followers"})
     @IsBooleanString()
     visibility:boolean;
-
-    @IsNotEmpty()
+    @ApiProperty({required:false,type:"string",description:"the tweet content it could be empty",})
     content:string;
-
     publishDate:Date;
-    
     imgPath:string;
-
+    @ApiProperty({required:true,default:"false",type:"boolean string",example:"false",description:"represent if the tweet is a retweet"})
+    @IsBooleanString()
+    isRetweet:boolean;
+    @ApiProperty({required:false,type:"ObjectId or string",description:"by default is not required but if this tweet is a retweet you should include"})
+    originalTweetId:string;
+    originalTweet:Tweet;
+    @ApiProperty({required:false,type:"string[binary]",description:"represent an image if you want to illustrate your tweet"})
     figure:[Object];
 }
--- backend/src/tweets/dto/interaction.dto.ts
+++ backend/src/tweets/dto/interaction.dto.ts
@@ -1,13 +1,15 @@
 import { IsNotEmpty, IsEnum } from 'class-validator';
 import { HttpStatus } from '@nestjs/common';
+import { ApiProperty } from '@nestjs/swagger';
 enum TweetInteractions {
   like = 'likes',
-  retweet = 'retweetBy',
   save = 'saved',
 }
 export default class InteractionDto {
+  @ApiProperty({required:true,description:"identify the active user",type:"ObjectId or string"})
   @IsNotEmpty()
   who: string;
+  @ApiProperty({required:true,enum:TweetInteractions,description:"represent what you want to do with the tweet",type:TweetInteractions})
   @IsEnum(TweetInteractions, {
     message: `The interaction should be: ${Object.values(
       TweetInteractions,
@@ -15,6 +17,7 @@ export default class InteractionDto {
     context: { statusCode: HttpStatus.BAD_REQUEST },
   })
   interaction: TweetInteractions;
+  @ApiProperty({required:true,description:"indentify the tweet you might change",type:"ObjectId or string"})
   @IsNotEmpty()
   tweetId:string;
 }
