bcf9a8a5b9b9a1f76c771e08d01403535ddbd5bb
sebas-baltor <sebastian_1321124218@uptecamac.edu.mx>
2023-06-06 18:21:25 -0600

    descrived model to swagger to show information

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,21 +1,59 @@
-import { IsEmail,IsLowercase,IsPhoneNumber,IsStrongPassword,IsNotEmpty,IsOptional } from "class-validator";
+import {
+  IsEmail,
+  IsLowercase,
+  IsPhoneNumber,
+  IsStrongPassword,
+  IsNotEmpty,
+  IsOptional,
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger/dist';
 
 export class CreateUserDto {
-    @IsNotEmpty()
-    name:string;
-    @IsNotEmpty()
-    bio:string;
-    @IsPhoneNumber("MX")
-    phone:string;
-    @IsEmail()
-    @IsLowercase()
-    email:string;
-    @IsStrongPassword()
-    password:string;
-    @IsOptional()
-    avatarPath:string;
-    @IsOptional()
-    backgroundPath:string;
-    avatar:[Object];
-    backdround:[Object];
+  @ApiProperty({ required: true })
+  @IsNotEmpty()
+  name: string;
+  @ApiProperty({ required: true })
+  @IsNotEmpty()
+  bio: string;
+  @ApiProperty({
+    required: true,
+    description: 'telephone number only in MX format',
+  })
+  @IsPhoneNumber('MX')
+  phone: string;
+  @ApiProperty({
+    required: true,
+    description: 'email address all in lower case',
+    example: 'myemail@gmail.com',
+  })
+  @IsEmail()
+  @IsLowercase()
+  email: string;
+  @ApiProperty({
+    required: true,
+    description:
+      'password must contain 8 characters in total, including numbers and special characters',
+  })
+  @IsStrongPassword()
+  password: string;
+  avatarPath: string;
+  backgroundPath: string;
+  @ApiProperty({
+    description:
+      'Its the object in form data who represent the profile picture only in [.jpeg .jpg .png .svg] format',
+    type: 'string',
+    format: 'binary',
+    required: true,
+    example:"img in [.jpeg, .png, .svg, .jpg] format"
+  })
+  avatar: [Object];
+  @ApiProperty({
+    description:
+      'Its the object in form data who represent the background picture only in  [.jpeg .jpg .png .svg] format',
+    type: 'string',
+    format: 'binary',
+    required: true,
+    example:"img in [.jpeg, .png, .svg, .jpg] format"
+  })
+  backdround: [Object];
 }
